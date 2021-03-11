9084540469956e89d72bd8f1f47e82579afc5b5a
lolicon <lolicon@mail.az>
2021-03-11 15:18:49 +0800

    fix

--- src/codegen-verify.main.kts
+++ src/codegen-verify.main.kts
@@ -6,10 +6,8 @@ import kotlinx.coroutines.flow.*
 fun String.eliminateUnknownLiteral(next: String) =
         listOf(this, next).joinToString { it.replace("""(?i)Unknown""".toRegex(), "") }
 
-fun main(args: Array<String>) {
-    val tracks = flow<String> {}
-    val invalidateHints = flow<String> {}
-    combine(tracks, invalidateHints, String::eliminateUnknownLiteral)
-}
+val tracks = flow<String> {}
+val invalidateHints = flow<String> {}
+combine(tracks, invalidateHints, String::eliminateUnknownLiteral)
 
 
